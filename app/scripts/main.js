40638ed7b895ef7a43c579c709e321d665efb7e0
Nando Rossi <talk@nan.do>
2016-11-23 17:59:23 +0100

    Custom, responsive slick
    Autoplay is commented out because it’s annoying when there’s tons on
    the page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,20 @@
 $( document ).ready(function() {
-    // console.log( "ready!" );
-    $('.work-gallery').slick({
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '20px',
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
+  // console.log( "ready!" );
+  $('.work-gallery').slick({
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    settings: {
+      // Hide arrows on small size
+      arrows: false
+    },
+    responsive: [
+      {
+        // This should be em but doesn't work
+        breakpoint: 640,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        settings: { arrows: true }
+      }
+    ]
+  });
 });
